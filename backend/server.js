305baf74ff26116b34825954c84a9e908127f6d0
Pratik Devkota <114279879+Pratik58D@users.noreply.github.com>
2025-06-10 15:13:10 +0545

    feat: sign in with google

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,15 @@ import connectDb from "./src/config/db.js";
 import userRouter from "./src/route/user.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import session from "express-session";
+import "./src/config/passport.js"
+import env from "dotenv";
 
+
+
+
+env.config();
 const app = express();
 const Port = 7000;
 
@@ -14,6 +22,11 @@ app.use(cors({
     origin : "http://localhost:5173" , 
     credentials : true
 }))
+
+// sign in with google
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.use("/api/user",userRouter)
--- backend/src/route/user.route.js
+++ backend/src/route/user.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { checkAuth, login, logout, signUp } from "../controller/user.controller.js";
 import { authenticate, refreshToken } from "../middleware/auth.js";
+import passport from "passport";
+import jwt from "jsonwebtoken"
 
 const router = express.Router();
 
@@ -9,6 +11,32 @@ router.post("/login",login);
 router.post("/refresh", refreshToken)
 router.post("/logout",logout)
 router.get("/check-auth",authenticate,checkAuth)
+// Google login route
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+
+// Google callback route
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false, failureRedirect: "/login" }),
+  (req, res) => {
+    const user = req.user;
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+    const refreshToken = jwt.sign({ id: user._id }, process.env.JWT_REFRESH_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false, // set to true in production (with HTTPS)
+      sameSite: "lax",
+    });
+
+      }
+);
 
 
 export default  router;
